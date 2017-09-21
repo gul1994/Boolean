a3c0e0d5fdda81634fa994ae6bcc6029d753a051
gul1994 <1may7777777@gmail.com>
2017-09-21 16:12:48 -0700

    my code

---
+++ src/conditions/Cups.java
@@ -0,0 +1,25 @@
+package conditions;
+
+public class Cups {
+
+	public static void main(String[] args) {
+      boolean white=true;
+      boolean black=false;
+      
+      System.out.println(white);
+
+      int n=22222;
+      int n1=6666;
+      
+    if(n>n1){
+    	System.out.println("Sprint demo meetings");
+    }  else{
+    	System.out.println("Automation tools");
+    
+    	
+    }
+
+	}
+	
+
+}
